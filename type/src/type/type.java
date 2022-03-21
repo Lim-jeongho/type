d8595a2127db728d0d49dbd29d845638a288747b
Lim-jeongho <limjh0387@gmail.com>
2022-03-21 22:32:43 +0900

    타입의 표현 범위

---
+++ type/src/type/type.java
@@ -0,0 +1,19 @@
+package type;
+
+public class type {
+
+	public static void main(String[] args) {
+		int i;
+		double d;
+		byte b;
+		
+		i = 7/4;
+		System.out.println(i);
+		d = 7/4;
+		System.out.println(d);
+		d= 7/(double)4;
+		System.out.println(d);
+
+	}
+
+}
